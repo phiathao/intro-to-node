ffb4e532c7bbde77b70ebe9161b5743a31237474
phiathao <phiat11@gmail.com>
2018-11-13 09:54:23 -0600

    package.json

---
+++ intro.js
@@ -0,0 +1,24 @@
+console.log('Hello world');
+
+let cohortName = 'Vega';
+console.log(cohortName);
+
+console.log( 2 / 5);
+
+let people = ['victoria', 'josh', 'vang'];
+people.forEach(function(person){
+    console.log('hello', person);
+});
+
+let pet = {
+    name: 'juniper',
+    type: 'cat',
+    breed: 'abysinian/mix'
+}
+console.log(`Dane's pet is ${pet.name}`);
+
+function add(x, y){
+    return x + y;
+}
+
+add(7, 3);
